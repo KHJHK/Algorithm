91da17a92c7505f5b9309447430d23773c028bb8
hicenter <86724344+KHJHK@users.noreply.github.com>
2023-02-04 02:00:48 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 448 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -1,10 +1,11 @@
-nums = list(map(int, input().split()))
-answer = max(nums) ** 3
-for i in range(3):
-    for j in range(i+1, 4):
-        for k in range(j+1, 5):
-            for n in range(min(nums[i], nums[j], nums[k]), (nums[i] * nums[j] * nums[k]) + 1):
-                if n % nums[i] == 0 and n % nums[j] == 0 and n % nums[k] == 0:
-                    answer = min(answer, n)
-                    break
-print(answer)
+a = list(map(int, input().split()))
+min = min(a)
+while True:
+    cnt = 0
+    for i in a:
+        if min % i == 0:
+            cnt += 1
+    if cnt > 2:
+        break
+    min += 1
+print(min)
