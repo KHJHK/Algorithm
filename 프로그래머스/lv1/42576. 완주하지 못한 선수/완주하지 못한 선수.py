0ef7dcc897882893a102f85a1bbf9ce6d1eff1d5
hicenter <86724344+KHJHK@users.noreply.github.com>
2023-05-04 18:50:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.40 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,12 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    answer = ''
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
+    if answer == '':
+        answer = participant.pop()
+    return answer
