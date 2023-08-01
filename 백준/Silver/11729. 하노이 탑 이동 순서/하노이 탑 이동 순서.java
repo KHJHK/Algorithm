ea6294e70353b158f374693519372dbcee6425ee
hicenter <86724344+KHJHK@users.noreply.github.com>
2023-08-01 09:38:54 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 348 ms, Memory: 94020 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,35 +1,43 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 
 public class Main {
-	static StringBuffer sb = new StringBuffer();
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int n = Integer.parseInt(br.readLine());
 		
-        System.out.println(hanoi(n, 1, 2, 3));
-        System.out.println(sb.toString());
-
+		
+		sb.append(hanoi(n) + "\n");
+        hanoi(n, 1, 2, 3);
+		
+		bw.write(sb.toString());
+		
+		bw.close();
 		br.close();
 	}
 	
-	static int hanoi(int n, int start, int mid, int end) {
+	static void hanoi(int n, int start, int mid, int end) {
 		if(n == 1) {	
 			sb.append(start + " " + end + "\n");
-			return 1;
+			return;
 		}
 		
-		int answer = 0;
-		answer += hanoi(n - 1, start, end, mid);
-		
-		answer += 1;
+		hanoi(n - 1, start, end, mid);
 		sb.append(start + " " + end + "\n");
+		hanoi(n - 1, mid, start, end);
 		
-		answer += hanoi(n - 1, mid, start, end);
-		
-		return answer;
+		return;
+	}
+	
+	static int hanoi(int n) {
+		return	(int)(Math.pow(2, n) - 1);
 	}
 }
