e934e52ed71b47a97d8c0b9330334f8ce207dce6
HuiJung <86724344+KHJHK@users.noreply.github.com>
2023-08-09 23:46:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 160 ms, Memory: 19008 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,58 +1,58 @@
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 
-	static int[] location;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int k = sc.nextInt();
-		int answer = 0;	//정답 출력용 변수, n = k 일때 이동이 0이므로 그대로 출력
-		
-		if(n > k) {
-			answer = n - k;
-		}else if(n < k) {
-			location = new int[2 * k + 1];
-			
-			bfs(n, k);
-			answer = location[k];
-		}
-		
-		System.out.println(answer);
-		sc.close();
-	}
-	
-	//bfs 사용
-	//시작점 좌표를 queue에 넣고 시작점에서 이동할 수 있는 모든 좌표 주기
-	//이동 후 좌표에서 다시 이동할 수 있는 모든 좌표 정보를 다시 queue에 넣기
-	//queue 빌때까지 반복
-	static void bfs(int n, int k) {
-		int[] a = location;
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(n);
-		while(!queue.isEmpty()) {
-			int current = queue.poll();
-			int front = current - 1;
-			int back = current + 1;
-			int twice = 2 * current;
-			
-			
-			if(front >= 1 && location[front] == 0) {
-				location[front] =  location[current] + 1;
-				queue.add(front);
-			}
-			if(back <= k && location[back] == 0) {
-				location[back] =  location[current] + 1;
-				queue.add(back);
-			}
-			if(twice <= 2 *k && location[twice] == 0) {
-				location[twice] =  location[current] + 1;
-				queue.add(twice);
-			}
-		}
-	}
+    static int[] location;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int answer = 0;	//정답 출력용 변수, n = k 일때 이동이 0이므로 그대로 출력
+
+        if(n > k) {
+            answer = n - k;
+        }else if(n < k) {
+            location = new int[2 * k + 1];
+
+            bfs(n, k);
+            answer = location[k];
+        }
+
+        System.out.println(answer);
+        sc.close();
+    }
+
+    //bfs 사용
+    //시작점 좌표를 queue에 넣고 시작점에서 이동할 수 있는 모든 좌표 주기
+    //이동 후 좌표에서 다시 이동할 수 있는 모든 좌표 정보를 다시 queue에 넣기
+    //queue 빌때까지 반복
+    static void bfs(int n, int k) {
+        int[] a = location;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(n);
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+            int front = current - 1;
+            int back = current + 1;
+            int twice = 2 * current;
+
+
+            if(front >= 1 && location[front] == 0) {
+                location[front] =  location[current] + 1;
+                queue.add(front);
+            }
+            if(back <= k && location[back] == 0) {
+                location[back] =  location[current] + 1;
+                queue.add(back);
+            }
+            if(twice <= 2 *k && location[twice] == 0) {
+                location[twice] =  location[current] + 1;
+                queue.add(twice);
+            }
+            if(front == k || back == k || twice == k)   break;
+        }
+    }
 
 }
