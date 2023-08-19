feae8d2e44434aefefb35d138c2b2b1ad71774bb
HuiJung <86724344+KHJHK@users.noreply.github.com>
2023-08-19 10:47:12 +0900

    [Gold IV] Title: N-Queen, Time: 2684 ms, Memory: 13900 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,46 +1,77 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 public class Main {
-    static int N, ans;
-    static int[] column;
-    static boolean visited[];   //각 행에 column 사용 확인
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        column = new int[N];
-        visited = new boolean[N];
-        nQuenn(0);
-
-        System.out.println(ans);
-        sc.close();
-    }
-
-    private static void nQuenn(int row){
-        if(row == N){
-            ans++;
-            return;
-        }
-
-        for (int col = 0; col < N; col++) {
-            if(!visited[col] && checkCross(row, col)){
-                column[row] = col;
-                visited[col] = !visited[col];
-                nQuenn(row + 1);
-                visited[col] = !visited[col];
-            }
-
-        }
-
-    }
-
-    private static boolean checkCross(int row, int col){
-        for (int bRow = 0; bRow < row; bRow++) {
-            if(row + col == column[bRow] + bRow || row - col == bRow - column[bRow])
-                return false;
-        }
-        return true;
-    }
-
+	static int n, ans;
+	static int visited[][];
+	
+	static int dy[] = {1,1,1};
+	static int dx[] = {-1,0,1};
+	static boolean OOB(int y,int x) {
+		return y<=0 || y>n || x<=0 || x>n;
+	}
+	
+	static void select(int cy,int cx) {
+		visited[cy][cx]+=1;
+		
+		for(int dir = 0; dir<3;dir++) {
+			int y = cy;
+			int x = cx;
+			
+			while(true) {
+				int ny = y + dy[dir];
+				int nx = x + dx[dir];
+				if(OOB(ny,nx))
+					break;
+				visited[ny][nx]+=1;
+				y = ny;
+				x = nx;
+			}
+		}
+	}
+	
+	static void restore(int cy,int cx) {
+		visited[cy][cx]-=1;
+		
+		for(int dir = 0; dir<3;dir++) {
+			int y = cy;
+			int x = cx;
+			
+			while(true) {
+				int ny = y + dy[dir];
+				int nx = x + dx[dir];
+				if(OOB(ny,nx))
+					break;
+				visited[ny][nx]-=1;
+				y = ny;
+				x = nx;
+			}
+		}
+	}
+	
+	
+	static void solve(int cur_y,int cnt) {
+		if(cnt == n) {
+//			System.out.println(cnt);
+			ans++;
+			return;
+		}
+		
+		for(int x=1; x<=n; x++) {
+			if(visited[cur_y][x] != 0)
+				continue;
+			select(cur_y,x);
+			solve(cur_y+1, cnt+1);
+			restore(cur_y, x);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		visited = new int[n+1][n+1];
+		solve(1,0);
+		System.out.println(ans);
+	}
 
 
 }
