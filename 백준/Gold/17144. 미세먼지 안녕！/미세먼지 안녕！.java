772819c91464963b2902352725c5d08618de59a3
HuiJung <86724344+KHJHK@users.noreply.github.com>
2023-10-11 08:44:49 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 268 ms, Memory: 23776 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,192 +1,210 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * 공기 청정기는 항상 1번 열에 설치, 두개의 행을 차지
+ * 공기 청정기가 설치 되지 	않은 칸에는 A(r,c)만큼의 미세먼지가 존재
+ * 
+ * 다음은 1초에 일어나는 일
+ * 1. 미세먼지 확산 => 인접한 네 방향으로 동시에 확산
+ * 1-1. 인접 방향에 공기청정기 존재 시 확산 x
+ * 1-2. 확산되는 양 A(r,c)/5, 소수점은 버림
+ * 1-3. (r,c)에 남은 미세먼지 양 : A(r,c)-(A(r,c)/5)*확산된 방향의 수
+ * 2. 공기청정기 작동
+ * 2-1. 공기청정기 바람
+ * 2-2. 위쪽 공기청정기 바람 : 반시계방향 순환, 아래쪽 공기청정기 바람 : 시계방향 순환
+ * 2-3. 바람 불면 미세먼지가 바람의 방향대로 모두 한 칸씩 이동
+ * 2-4. 공기청정기로 들어간 미세먼지는 정화
+ * 
+ * 해결 방법
+ * 미세먼지 확산 : BFS를 이용하여 확산 -> 오늘 했던 벽돌깨기와 같이 연쇄적으로 일어나므로 맵을 복사하여 사용
+ * 공기청정기는 배열 돌리기
+ * 
+ * 
+ * @author SSAFY
+ *
+ */
+
 public class Main {
-	/**
-	 * Question 1. map의 Row, Col, Time이 주어진다 2. map이 주어진다 - 공기청정기(-1), 먼지(1 이상 수)
-	 * 주어짐 3. 주어진 행동을 수행한다 3.1 미세먼지가 확산된다. 확산은 미세먼지가 있는 모든 칸에서 동시에 일어난다. - (r, c)에
-	 * 있는 미세먼지는 인접한 네 방향으로 확산된다. - 인접한 방향에 공기청정기가 있거나, 칸이 없으면 그 방향으로는 확산이 일어나지 않는다.
-	 * - 확산되는 양은 A(r,c)/5이고 소수점은 버린다. - (r, c)에 남은 미세먼지의 양은 Ar,c - (Ar,c/5)×(확산된 방향의
-	 * 개수) 이다. 3.2 공기청정기가 작동한다. - 공기청정기에서는 바람이 나온다. - 위쪽 공기청정기의 바람은 반시계방향으로 순환하고,
-	 * 아래쪽 공기청정기의 바람은 시계방향으로 순환한다. - 바람이 불면 미세먼지가 바람의 방향대로 모두 한 칸씩 이동한다. - 공기청정기에서
-	 * 부는 바람은 미세먼지가 없는 바람이고, 공기청정기로 들어간 미세먼지는 모두 정화된다.
-	 * 
-	 * Answer 주어진 행동 대로 구현 - 단, 먼지 확산 이전 값을 기준으로 먼지가 확산 되기 때문에 먼지 확산 map과 먼지 확산 이전
-	 * map을 따로 만들어 준다.
-	 */
-
-	static int R, C, T;
-	static int dir, cleanerRow; // cleanerCol은 공기청정기의 아래부분 row
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
-	static int map1[][], map2[][];
-
-	public static void main(String[] args) throws IOException {
+	
+	
+	
+	static class Node{
+		int r;
+		int c;
+		public Node(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
+	
+	static int R,C,T;	//R: 행, C: 열, T: 시간
+	static int[][] map;
+	static List<Node> list;
+	static boolean[][] visited;
+	
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		StringTokenizer st;
+		
+		
+		st = new StringTokenizer(br.readLine());
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
-
-		map1 = new int[R][C];
-		map2 = new int[R][C];
-
-		for (int r = 0; r < R; r++) {
+		
+		map = new int[R][C];
+		list = new ArrayList<Node>();
+		
+		for(int i=0;i<R;i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < C; c++) {
-				map1[r][c] = Integer.parseInt(st.nextToken());
-				if (map1[r][c] == -1) {
-					cleanerRow = r;
-				}
+			for(int j=0;j<C;j++) {
+				map[i][j]=Integer.parseInt(st.nextToken());
+				if(map[i][j]==-1) list.add(new Node(i,j));
 			}
 		}
-
-		int pickMap = 0;
-		for (int t = 0; t < T; t++) {
-			pickMap = t % 2;
-			spreadDust(pickMap);
-			moveDust(pickMap);
+		
+		
+		//T로 감싸기
+		
+		for(int i=0;i<T;i++) {
+			simul();
 		}
-
-		int answer = 2;
-		if (pickMap == 0) {
-			for (int[] m : map2) {
-				for (int i = 0; i < C; i++) {
-					answer += m[i];
-				}
-			}
-		} else {
-			for (int[] m : map1) {
-				for (int i = 0; i < C; i++) {
-					answer += m[i];
-				}
-			}
-		}
-
-		System.out.println(answer);
+		
+		
+		System.out.println(count());
+		
 	}
-
-	private static void spreadDust(int pickMap) {
-		int[][] map;
-		int[][] nextMap;
-
-		if (pickMap == 0) {
-			map = map1;
-			nextMap = map2;
-		} else {
-			map = map2;
-			nextMap = map1;
+	
+	private static int count() {
+		int num = 0;
+		for(int i=0;i<R;i++) {
+			for(int j=0;j<C;j++) {
+				if(map[i][j]>0) num+=map[i][j];
+			}
 		}
-		for (int[] nm : nextMap) {
-			Arrays.fill(nm, 0);
+		return num;
+	}
+	
+	//반시계 방향
+	static final int[] cdr = {-1,0,1,0};
+	static final int[] cdc = {0,1,0,-1};
+	private static void crotate() {
+		int dir=0;
+		int r = list.get(0).r;
+		int c = list.get(0).c;
+		
+		while(dir<4) {
+			int nr = r+cdr[dir];
+			int nc = c+cdc[dir];
+			
+			if(nr>=0 && nr<=list.get(0).r && nc>=list.get(0).c && nc<C) {
+				map[r][c] = map[nr][nc];
+				r = nr;
+				c = nc;
+				if(dir==3 && nc==1) {
+					map[list.get(0).r][1]=0;
+					break;
+				}
+			}else {
+				dir++;
+			}
 		}
-		nextMap[cleanerRow - 1][0] = -1;
-		nextMap[cleanerRow][0] = -1;
-
-		for (int r = 0; r < R; r++) {
-			for (int c = 0; c < C; c++) {
-				// 먼지면 퍼트리기
-				if (map[r][c] > 0) {
-					int spreadedDust = map[r][c] / 5;
-					nextMap[r][c] += map[r][c];
-					// 4방향으로 퍼트리기
-					for (int d = 0; d < 4; d++) {
-						int nextR = r + dr[d];
-						int nextC = c + dc[d];
-
-						if (checkBoundary(nextR, nextC) && map[nextR][nextC] != -1) {
-							nextMap[nextR][nextC] += spreadedDust;
-							nextMap[r][c] -= spreadedDust;
-						}
+		
+		map[list.get(0).r][list.get(0).c]=-1;
+	}
+	
+	//시계 방향
+	static final int[] dr = {1,0,-1,0};
+	static final int[] dc = {0,1,0,-1};
+	private static void rotate() {
+		int dir=0;
+		int r = list.get(1).r;
+		int c = list.get(1).c;
+		
+		while(dir<4) {
+			int nr = r+dr[dir];
+			int nc = c+dc[dir];
+			
+			if(nr>=list.get(1).r && nr<R && nc>=list.get(1).c && nc<C) {
+				
+				map[r][c] = map[nr][nc];
+				r = nr;
+				c = nc;
+				if(dir==3 && nc==1) {
+					map[list.get(1).r][1]=0;
+					break;
+				}
+			}else {
+				dir++;
+			}
+		}
+		map[list.get(1).r][list.get(1).c]=-1;
+	}
+	
+	private static void simul() {
+//		print();
+		diffuse();
+//		System.out.println("============================확산 이후===========================================");
+//		print();
+		crotate();
+		rotate();
+//		System.out.println("============================회전 이후===========================================");
+//		print();
+		
+//		System.out.println("=====================한턴 끝=============================");
+	}
+	
+	private static void diffuse() {
+		int[][] newMap = new int[R][C];
+		
+		for(int i=0;i<R;i++) {
+			for(int j=0;j<C;j++) {
+				if(map[i][j]==0 || map[i][j]==-1) continue;
+				int value = map[i][j]/5;
+				for(int d=0;d<4;d++) {
+					int nr = i+dr[d];
+					int nc = j+dc[d];
+					
+					if(isInRange(nr, nc) && map[nr][nc]!=-1) {
+						newMap[i][j] -= value;
+						newMap[nr][nc]+=value;
 					}
 				}
 			}
 		}
-
-	}
-
-	private static void moveDust(int pickMap) {
-		int[][] map;
-
-		if (pickMap == 0) {
-			map = map2;
-		} else {
-			map = map1;
-		}
-
-		// 상단 먼지 이동
-		int r = 0;
-		int c = 0;
-		int temp = map[r][c];
-		int total = 2 * cleanerRow + 2 * C - 5;
-		dir = 1;
-
-		for (int cnt = 1; cnt <= total; cnt++) {
-			if (cnt == C) {
-				dir = 2;
-			} else if (cnt == C + cleanerRow - 1) {
-				dir = 3;
-			} else if (cnt == 2 * C + cleanerRow - 2) {
-				dir = 0;
-			}
-
-			int nextR = r + dr[dir];
-			int nextC = c + dc[dir];
-
-			if (map[r][c] != -1) {
-				if (map[nextR][nextC] == -1) {
-					map[r][c] = 0;
-				} else {
-					map[r][c] = map[nextR][nextC];
+		
+		for(int i=0;i<R;i++) {
+			for(int j=0;j<C;j++) {
+				if(newMap[i][j]!=0) {
+					map[i][j]+=newMap[i][j];
 				}
 			}
-
-			r = nextR;
-			c = nextC;
-
 		}
-		map[r][c] = temp;
-
-		// 하단 먼지 이동
-		r = R - 1;
-		c = 0;
-		temp = map[r][c];
-		total = 2 * (R - cleanerRow) + 2 * C - 5;
-		dir = 1;
-
-		for (int cnt = 1; cnt <= total; cnt++) {
-			if (cnt == C) {
-				dir = 0;
-			} else if (cnt == C + R - cleanerRow - 1) {
-				dir = 3;
-			} else if (cnt == 2 * C + R - cleanerRow - 2) {
-				dir = 2;
-			}
-
-			int nextR = r + dr[dir];
-			int nextC = c + dc[dir];
-
-			if (map[r][c] != -1) {
-				if (map[nextR][nextC] == -1) {
-					map[r][c] = 0;
-				} else {
-					map[r][c] = map[nextR][nextC];
-				}
-			}
-
-			r = nextR;
-			c = nextC;
-
-		}
-		map[r][c] = temp;
 	}
-
-	private static boolean checkBoundary(int row, int col) {
-		return row >= 0 && row < R && col >= 0 && col < C;
+	
+	private static boolean isInRange(int x, int y) {
+		return x>=0 && y>=0 && x<R && y<C;
+	}
+	
+	private static void copy(int[][] map, int[][] newMap) {
+		for(int i=0;i<R;i++) {
+			System.arraycopy(map[i], 0, newMap[i], 0, C);
+		}
+	}
+	
+	private static void print() {
+		for(int i=0;i<R;i++) {
+			for(int j=0;j<C;j++) {
+				System.out.print(map[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
