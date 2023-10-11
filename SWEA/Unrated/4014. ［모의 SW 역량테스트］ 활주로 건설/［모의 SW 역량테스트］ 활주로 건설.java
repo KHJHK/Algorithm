fcf2899bd9a156b6d1d3e4427414036734a0a8df
HuiJung <86724344+KHJHK@users.noreply.github.com>
2023-10-11 17:42:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 123 ms, Memory: 23,356 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -1,123 +1,131 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
+
+	static int N,X;
+	static int[][] map, rMap;
 	
-	static int N, X, answer;
-	static int[][] map;
-	static boolean[] visited;
-	
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
-		int testCase = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
-		for (int tc = 1; tc <= testCase; tc++) {
-			answer = 0;
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
+		for(int tc=1;tc<=T;tc++) {
+			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			X = Integer.parseInt(st.nextToken());
 			
 			map = new int[N][N];
+			rMap = new int[N][N];
 			
-			for (int r = 0; r < N; r++) {
+			for(int i=0;i<N;i++) {
 				st = new StringTokenizer(br.readLine());
-				for (int c = 0; c < N; c++) {
-					map[r][c] = Integer.parseInt(st.nextToken());
+				for(int j=0;j<N;j++) {
+					int a = Integer.parseInt(st.nextToken());
+					map[i][j] = a;
+					rMap[j][i] = a;
 				}
 			}
 			
+
 			
-			//행 검사
-			for (int r = 0; r < N; r++) {
-				if(checkLoad(map[r])) {
-					answer++;
-				}
+			int answer = 0;
+			for(int i=0;i<N;i++) {
+				if(check(map[i]))answer++;
+				if(check(rMap[i]))answer++;
 			}
-			//열 검사
-			for (int c = 0; c < N; c++) {
-				int[] load = new int[N];
-				for (int r = 0; r < N; r++) {
-					load[r] = map[r][c];
-				}
-				
-				if(checkLoad(load)) {
-					answer++;
-				}
-			}
-			
-			sb.append("#").append(tc).append(" ").append(answer).append("\n");
+			sb.append("#").append(tc).append(" ").append(answer).append(System.lineSeparator());
 		}
 		System.out.println(sb);
 		
-		
 	}
-
-
-	private static boolean checkLoad(int[] load) {
-		visited = new boolean[N];
+	
+	private static boolean check(int[] arr) {
+		//경사로 가능한지 판별해주는 애
+		boolean isOk = true;
+		boolean[] visited = new boolean[arr.length];
 		
-		for (int i = 0; i < N - 1; i++) {
-			int before = load[i];
-			int after = load[i + 1];
-			
-			//고저가 다르면
-			if(before != after) {
-				int cnt = 0;
-				int check = 0;
-				int checkIdx = i;
-				//뒷칸이 한 칸 낮을 때
-				if(before == after + 1) {
-					while(cnt != X) {
-						cnt++;
-						checkIdx += 1;
-						if(!checkBoundary(checkIdx)) {
-							return false;
-						}
-						
-						check = load[checkIdx];
-						
-						if(check != after || visited[checkIdx]) {
-							return false;
-						}
-						
-						visited[checkIdx] = true;
-					}
-				}else if(before == after - 1) {//뒷칸이 한 칸 높을 때
-					checkIdx += 1;
-					while(cnt != X) {
-						cnt++;
-						checkIdx -= 1;
-						if(!checkBoundary(checkIdx)) {
-							return false;
-						}
-						
-						check = load[checkIdx];
-						
-						if(check != before || visited[checkIdx]) {
-							return false;
-						}
-						
-						visited[checkIdx] = true;
-					}
-				}else {
+		for(int i=0;i<N-1;i++) {
+			//방향 높이가 다르면
+			if(arr[i]!=arr[i+1]) {
+				//높이 차이가 2이상이면 불가능하다고 하고 바로 탈출
+				if(Math.abs(arr[i+1]-arr[i])>=2) {
 					return false;
+				}
+				//만약 오른쪽이 더 크다면
+				if(arr[i+1]>arr[i]) {
+					if(i+1-X>=0) {
+						boolean isSameHeight = true;
+						for(int j=i; j>=i-X+1; j--) {
+							if(arr[j]!=arr[i]) {
+								isSameHeight=false;
+								break;
+							}
+						}
+						if(isSameHeight) {
+							for(int j=i; j>=i-X+1; j--) {
+								if(visited[j]) {
+									return false;
+								}
+								visited[j]=true;
+							}
+						}
+						if(!isSameHeight)return false;
+					}
+					else {
+						return false;
+					}
+				}
+				//만약 왼쪽이 더 크다면
+				if(arr[i]>arr[i+1]) {
+					//만약 i+X가 N안에 있다면
+					if(i+X < N) {
+						//만약 높이 변동이 없다면
+						boolean isSameHeight = true;
+						for(int j=i+1;j<=i+X;j++) {
+							if(arr[j]!=arr[i+1]) {
+								isSameHeight = false;
+								break;
+							}
+						}
+						if(isSameHeight) {
+							for(int j=i+1;j<=i+X;j++) {
+								if(visited[j]) {
+									return false;
+								}
+								visited[j]=true;
+							}
+							
+						}
+						if(!isSameHeight) {
+							isOk=false;
+						}
+					}
+					else {
+						return false;
+					}
 				}
 				
 			}
 		}
 		
-		return true;
+		if(isOk==true)return true;
+		return false;
 	}
+
 	
-	private static boolean checkBoundary(int idx) {
-		return idx >= 0 && idx < N;
+	private static void print() {
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				System.out.print(map[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
