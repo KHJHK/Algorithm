75c7d185010b4d5de4347348186077adb1cf05f8
HuiJung <86724344+KHJHK@users.noreply.github.com>
2023-10-12 15:52:48 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 138 ms, Memory: 21,504 KB -BaekjoonHub

--- SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
+++ SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -1,112 +1,107 @@
 import java.io.BufferedReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.IOException;
 import java.io.InputStreamReader;
-
-/**
- * 숫자는 움직이지 않음
- * 연산카드를 다양한 순서로 조합하여 수식을 계산
- * 연산자의 우선 순위는 고려하지 않고 왼쪽에서 오른쪽 차례대로 계산
- * 
- * next Permutation을 이용하여 연산카드 조합을 계산
- * 각 계산마다 수식 계산해주면서 최솟값 최댓값 판볋하여 갱신
- * 
- * @author SSAFY
- *
- */
+import java.util.StringTokenizer;
 
 public class Solution {
-
-	static int N;
-	static int numbers[];
-	static int[] operator;
-	static int[] out;
-	static int min, max;
+	static int N, min, max;
+	static int[] nums, operators;
 	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testCase = Integer.parseInt(br.readLine());
 		
-		int T = Integer.parseInt(br.readLine());
-		for(int tc=1;tc<=T;tc++) {
-			N = Integer.parseInt(br.readLine());
-			
-			numbers = new int[N];
-			operator = new int[4];
-			out = new int[N-1];
-			
-			st = new StringTokenizer(br.readLine());
-			
-			for(int i=0;i<4;i++) {
-				operator[i] = Integer.parseInt(st.nextToken()); 
-			}
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i=0;i<N;i++) {
-				numbers[i]=Integer.parseInt(st.nextToken());
-			}
-			
+		for (int tc = 1; tc <= testCase; tc++) {
 			min = Integer.MAX_VALUE;
 			max = Integer.MIN_VALUE;
+			N = Integer.parseInt(br.readLine());
+			nums = new int[N];
+			operators = new int[N - 1];
 			
-			permutation(0);
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int idx = 0;
 			
-			sb.append("#").append(tc).append(" ").append(max-min).append(System.lineSeparator());
+			for (int i = 0; i < 4; i++) {
+				int op = Integer.parseInt(st.nextToken());
+				int cnt = 0;
+				
+				while(cnt++ < op) {
+					operators[idx++] = i;
+				}
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				nums[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			do {
+				operating();
+			}while(np());
+			
+			sb.append("#").append(tc).append(" ").append(max - min).append("\n");
 		}
 		System.out.println(sb);
 	}
-	
-	private static void permutation(int depth) {
-		if(!isAble(out, depth))return;
-		
-		//기저 조건
-		if(depth==N-1) {
-			int cal = calculate(out);
-//			System.out.println(Arrays.toString(out));
-			min = Math.min(min, cal);
-			max = Math.max(max, cal);
-			return;
+
+
+	//다음 큰 순열이 있으면 true, 없으면 false
+		private static boolean np() {
+			//i, j, k 모두 맨 뒤에서 부터 시작
+			//비교하는 부분 모두 i-1부터, 
+			
+			int N = operators.length;
+			//step1. 꼭대기를(i) 찾는다. 꼭대기를 통해 교환위치(i-1)찾는다
+			int i = N-1;
+			while(i>0 && operators[i-1]>=operators[i]) --i;
+			
+			//맨앞이다 절벽 - 다음은 없어 너가 가장 커
+			if(i==0) {return false;}
+			
+			//step2. i-1위치값과 교환할 큰 값 찾기
+			int j = N-1;
+			
+			//i-1과 교환할 것은 항상 있다
+			while(operators[i-1]>=operators[j]) --j;
+			
+			//step3. i-1위치값과 j위치값 교환
+			swap(i-1, j);
+			
+			//step4. 꼭대기(i)부터 맨 뒤까지 내림차순 형태의 순열을 오름차순으로 처리
+			int k = N-1;
+			while(i<k) {
+				swap(i++, k--);
+			}
+			
+			return true;		
+		}
+	    
+	    //위치 교환 함수
+	    private static void swap(int i, int j) {
+			int temp = operators[i];
+			operators[i] = operators[j];
+			operators[j] = temp;
+		}
+
+
+	private static void operating() {
+		int result = nums[0];
+		for (int idx = 0; idx < N - 1; idx++) {
+			if(operators[idx] == 0) {
+				result += nums[idx + 1];
+			}else if(operators[idx] == 1) {
+				result -= nums[idx + 1];
+			}else if(operators[idx] == 2) {
+				result *= nums[idx + 1];
+			}else if(operators[idx] == 3) {
+				result /= nums[idx + 1];
+			}
 		}
 		
-		for(int i=0;i<4;i++) {
-			out[depth]=i;
-			permutation(depth+1);
-		}
-	}
-	
-	private static int calculate(int[] arr) {
-		int start = numbers[0];
-		int result = start;
-		
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i]==0) {	//덧셈
-				result = result+numbers[i+1];
-			}
-			else if(arr[i]==1) {	//뺄셈
-				result = result-numbers[i+1];
-			}
-			else if(arr[i]==2) {	//곱셈
-				result = result*numbers[i+1];
-			}
-			else if(arr[i]==3) {	//나눗셈
-				result = result/numbers[i+1];
-			}
-		}
-		
-		return result;
-	}
-	
-	private static boolean isAble(int[] arr, int depth) {
-		int[] count = new int[4];
-		
-		for(int i=0;i<depth;i++) {
-			count[arr[i]]+=1;
-			if(operator[arr[i]]<count[arr[i]]) return false;
-		}
-		
-		return true;
-		
+		min = Math.min(min, result);
+		max = Math.max(max, result);
 	}
 
 }
