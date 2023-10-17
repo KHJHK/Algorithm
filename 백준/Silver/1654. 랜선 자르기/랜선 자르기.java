5156c00fca28085376620ced089c733c31e2a4c5
HuiJung <86724344+KHJHK@users.noreply.github.com>
2023-10-17 10:23:31 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 15300 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static int K, N;
+	static int[] nums;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		K = Integer.parseInt(input[0]);
+		N = Integer.parseInt(input[1]);
+		nums = new int[K];
+		int max = 0;
+		
+		for (int k = 0; k < K; k++) {
+			nums[k] = Integer.parseInt(br.readLine());
+			max = Math.max(max, nums[k]);
+		}
+				
+		long answer = binarySearch(1, max);
+		
+		System.out.println(answer);
+		
+	}
+
+	private static long binarySearch(long start, long end) {
+		long mid = (end + start) / 2;
+		int cnt = 0;
+		
+		while(start <= end) {
+			mid = (end + start) / 2;
+			cnt = 0;
+			
+			for (int num : nums) {
+				cnt += num / mid;
+			}
+			
+			if(cnt < N) {
+				end = mid - 1;
+			}else if(cnt >= N) {
+				cnt = 0;
+				for (int num : nums) {
+					cnt += num / (mid + 1);
+				}
+				
+				if(cnt >= N) {
+					start = mid + 1;
+				}else {
+					break;
+				}
+			}
+		}
+		
+		return mid;
+	}
+
+}
