5fc096e7197d2a989e2014e899a13e4eefb8f2ea
HuiJung <86724344+KHJHK@users.noreply.github.com>
2024-12-02 03:30:30 +0900

    [Gold II] Title: 소풍, Time: 196 ms, Memory: 18136 KB -BaekjoonHub

--- 백준/Gold/2026. 소풍/소풍.java
+++ 백준/Gold/2026. 소풍/소풍.java
@@ -1,6 +1,22 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * - 문제
+ * N명 중 K명 소풍을 보냄, 모두 친구사이여야함
+ * 
+ * - 풀이
+ * 백트래킹
+ * 1. 1번부터 시작, bfs or dfs
+ * 2. 현재 뽑힌 친구 visited 처리
+ * 3. 현재 친구와 이어진 다른 친구들 visited 여부 카운트
+ * 4. 현재 depth - 1(본인 제외) != visited 카운트 개수 일 경우 break
+ * 5. 1번에서 뽑은 친구 제외, 다음 번호 친구부터 다시 반복
+ * 6. 1~5 반복하며 depth가 K이면 종료
+ * 
+ * 불가능한 경우
+*/
+
 public class Main {
 	static int K, N; // 뽑는 수, 총 인원
 	static int impossibleNum = 0; //그룹이 불가능한 경우 저장, 해당 번호 이하의 친구들은 친구가 불가능함
