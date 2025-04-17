6eae6f41f12517ed72c3acc4626f8e0a2fa4abb0
HuiJung <86724344+KHJHK@users.noreply.github.com>
2025-04-18 00:15:55 +0900

    [Gold II] Title: 계보 복원가 호석, Time: 652 ms, Memory: 131012 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.java
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.java
@@ -5,10 +5,9 @@ public class Main {
 	static int N, M;
 	static String[] names; //이름 저장
 	static int[] parents; //부모의 idx 저장
-	static boolean[] visited; //bfs용
 	static Map<String, Integer> nameToIndex= new HashMap<>(); //key가 이름 value가 idx
 	static Map<Integer, String> indexToName = new HashMap<>(); //key가 idx value가 이름
-	static Set<Integer> ancestorSet = new HashSet<>(); //조상이 될 수 있는 집합군
+	static int[] inDegree; //진입차수
 	static ArrayList<Integer>[] graph; //연결 관계
 	static ArrayList<Integer>[] descendants; //자식들 저장
 	static StringBuilder sb = new StringBuilder(); //출력용 StringBuilder
@@ -17,8 +16,8 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		names = new String[N];
+		inDegree = new int[N];
 		graph = new ArrayList[N];
-		visited = new boolean[N];
 		parents = new int[N];
 		descendants = new ArrayList[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,28 +26,30 @@ public class Main {
 		for(int i = 0; i < N; i++) {
 			nameToIndex.put(names[i], i);
 			indexToName.put(i, names[i]);
-			ancestorSet.add(i);
 			graph[i] = new ArrayList<>();
 			descendants[i] = new ArrayList<>();
 		}
 		M = Integer.parseInt(br.readLine());
+		
+		int ancestorCnt = N;
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			String descendant = st.nextToken();
 			String ancestor = st.nextToken();
-			ancestorSet.remove(nameToIndex.get(descendant));
+			if(inDegree[nameToIndex.get(descendant)] == 0) ancestorCnt--;
+			inDegree[nameToIndex.get(descendant)]++;
 			graph[nameToIndex.get(ancestor)].add(nameToIndex.get(descendant));
 		}
 		
 		//가문의 수 및 시조 저장
-		sb.append(ancestorSet.size()).append('\n');
-		ArrayList<Integer> ancestorList = new ArrayList<>(ancestorSet);
-		Collections.sort(ancestorList);
-		for(int i : ancestorList) {
+		sb.append(ancestorCnt).append('\n');
+		for(int i = 0; i < N; i++) {
+			if(inDegree[i] != 0) continue;
 			sb.append(indexToName.get(i)).append(' ');
-			BFS(i); //가문 찾기
 		}
 		sb.append('\n');
+		
+		topologySort(); //가문 찾기
 		
 		//부모-자식 관계 저장
 		for(int p = 0; p < N; p++) {
@@ -67,17 +68,21 @@ public class Main {
 		System.out.println(sb);
 	}
 	
-	static void BFS(int ancestor) {
+	static void topologySort() {
 		Queue<Integer> q = new ArrayDeque<>();
-		visited[ancestor] = true;
-		parents[ancestor] = -1;
-		q.offer(ancestor);
+		for(int i = 0; i < N; i++) {
+			if(inDegree[i] == 0) {
+				q.offer(i);
+				parents[i] = -1;
+			}
+		}
+		
 		while(!q.isEmpty()) {
 			int now = q.poll();
 			for(int next : graph[now]) {
-				parents[next] = now;
-				if(!visited[next]) {
-					visited[next] = true;
+				inDegree[next]--;
+				if(inDegree[next] == 0) {
+					parents[next] = now;
 					q.offer(next);
 				}
 			}
