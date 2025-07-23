33b728e444511becae750230075aa0b34ea9793b
HuiJung <86724344+KHJHK@users.noreply.github.com>
2025-07-24 00:43:36 +0900

    [Gold IV] Title: 인구 이동, Time: 316 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,101 +1,113 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int N, L, R;
-	static int groupNum;
-	static boolean flag;
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, 1, 0, -1};
-	static int[] groupPopulations;
-	static int[][] board;
-	static int[][] groupBoard;
-	static boolean[][] visited;
-	
-	public static void main(String[] args) throws IOException{
+	static int day, org;
+	static boolean isMoveAble;
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
+	static int[][] population;
+	static int[][] map, mapOrg;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
-		board = new int[N][N];
-		groupBoard = new int[N][N];
-		visited = new boolean[N][N];
-		groupPopulations = new int[(N * N) + 1];
-		
-		for(int i = 0; i < N; i++) {
+
+		map = new int[N][N];
+		mapOrg = new int[N][N];
+		population = new int[N * N][2];
+
+		for (int r = 0; r < N; r++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < N; j++) board[i][j] = Integer.parseInt(st.nextToken());
+			for (int c = 0; c < N; c++) {
+				map[r][c] = Integer.parseInt(st.nextToken());
+				mapOrg[r][c] = -1;
+			}
 		}
 		
-		int day = 0;
-		while(true) {
-			boolean isEnd = true;
-			groupNum = 1;
-			flag = !flag;
-			//그룹 나누기
-			for(int r = 0; r < N; r++) {
-				for(int c = 0; c < N; c++) {
-					if(visited[r][c] == flag) continue;
-					makeGroup(r, c);
-					groupNum++;
+		isMoveAble = true;
+		
+		while(isMoveAble) {
+			isMoveAble = false;
+
+			// 조합 나누기
+			org = 0;
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if (mapOrg[r][c] == -1) {
+						dfs(r, c);
+						org++;
+					}
+				}
+			}
+
+//			for (int[] i : mapOrg) {
+//				System.out.println(Arrays.toString(i));
+//			}
+
+			// 인구 이동
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if(mapOrg[r][c] == -1) {
+						continue;
+					}
+					int change = population[mapOrg[r][c]][0] / population[mapOrg[r][c]][1];
+					if(map[r][c] != change) {
+						map[r][c] = change;
+						isMoveAble = true;
+					}
+					mapOrg[r][c] = -1;
 				}
 			}
 			
-			//인구수 통일
-			for(int r = 0; r < N; r++) {
-				for(int c = 0; c < N; c++) {
-					int num = groupBoard[r][c];
-					int population = groupPopulations[num];
-					if(board[r][c] != population) isEnd = false;
-					board[r][c] = population;
+			if(!isMoveAble) {
+				break;
+			}
+			
+			// 인구수 초기화
+			for (int i = 0; i < org; i++) {
+				for (int j = 0; j < 2; j++) {
+					population[i][j] = 0;
 				}
 			}
 			
-			if(isEnd) break;
 			day++;
 		}
 		
 		System.out.println(day);
 	}
-	
-	static void makeGroup(int sr, int sc) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {sr, sc, board[sr][sc]});
-		visited[sr][sc] = flag;
-		groupBoard[sr][sc] = groupNum;
-		
-		int sum = board[sr][sc];
-		int cnt = 1;
-		
-		while(!q.isEmpty()) {
-			int[] now = q.poll();
-			int r = now[0];
-			int c = now[1];
-			int population = now[2];
-			
-			for(int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				
-				if(OOB(nr, nc)) continue;
-				if(visited[nr][nc] == flag) continue;
-				
-				int diff = Math.abs(population - board[nr][nc]);
-				if(L > diff || R < diff) continue;
-				
-				q.offer(new int[] {nr, nc, board[nr][nc]});
-				visited[nr][nc] = flag;
-				sum += board[nr][nc];
-				cnt++;
-				groupBoard[nr][nc] = groupNum;
+
+	private static void dfs(int r, int c) {
+		int nextR = r;
+		int nextC = c;
+
+		for (int d = 0; d < 4; d++) {
+			nextR = r + dr[d];
+			nextC = c + dc[d];
+
+			if (checkBoundary(nextR, nextC) && mapOrg[nextR][nextC] == -1 && checkPopulationDiff(r, c, nextR, nextC)) {
+				mapOrg[nextR][nextC] = org;
+				population[org][0] += map[nextR][nextC]; //인구 수
+				population[org][1]++; //도시 수
+				dfs(nextR, nextC);
 			}
 		}
-		
-		groupPopulations[groupNum] = sum / cnt;
 	}
-	
-	static boolean OOB(int r, int c) { return r < 0 || r >= N || c < 0 || c >= N; }
+
+	private static boolean checkPopulationDiff(int row, int col, int nextR, int nextC) {
+		int diff = Math.abs(map[row][col] - map[nextR][nextC]);
+		return L <= diff && diff <= R;
+	}
+
+	private static boolean checkBoundary(int row, int col) {
+		return row >= 0 && row < N && col >= 0 && col < N;
+	}
 
 }
